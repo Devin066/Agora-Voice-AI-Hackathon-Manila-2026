326f4152ad70ae4de6e0fa1293732154517e21c6
SntJhn <jagdevera@gmail.com>
2026-03-16 12:07:22 +0800

    alventur init

---
+++ submissions/Alventur Solutions/Clara/Clara/ClaraApp.swift
@@ -0,0 +1,32 @@
+//
+//  ClaraApp.swift
+//  Clara
+//
+//  Created by Ali on 3/16/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ClaraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
